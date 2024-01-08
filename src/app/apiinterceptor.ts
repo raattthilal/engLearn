ff6f60e87aaed4e1a7179b15bebbeb2e33fdd2d9
raattthilal <muhammedhilal.a@gmail.com>
2024-01-08 13:38:39 +0530

    https

--- src/app/apiinterceptor.ts
+++ src/app/apiinterceptor.ts
@@ -9,7 +9,7 @@ export class Apiinterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     //    const baseUrl = "http://13.233.161.93:";
-        const baseUrl = "http://65.1.247.168:";
+        const baseUrl = "https://65.1.247.168:";
         // const baseUrl = "http://localhost:";
         const apiReq = req.clone({ url: `${baseUrl}${req.url}` });
         return next.handle(apiReq);
